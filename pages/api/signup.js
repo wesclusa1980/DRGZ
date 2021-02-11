3be5e92aec39598d60608cc3b72dd73667c92438
uzairbangee <uzairbangee@gmail.com>
2021-02-11 23:13:21 +0500

    feat: add some changes

--- pages/api/signup.js
+++ pages/api/signup.js
@@ -1,6 +1,13 @@
 import { query as q } from "faunadb";
 import { guestClient } from "../../utils/fauna-client";
 import { setAuthCookie } from "../../utils/auth-cookies";
+import {client, tokenId} from "../../utils/hedera-treasury";
+const {
+  AccountCreateTransaction,
+  AccountId,
+  TokenAssociateTransaction,
+  PrivateKey
+} = require("@hashgraph/sdk");
 
 export default async function signup(req, res) {
   const { name, email, password } = req.body;
@@ -18,6 +25,42 @@ export default async function signup(req, res) {
     if (existingEmail) {
       return res.status(400).send(`Email ${email} already exists`);
     }
+    ////Public and Private Area
+    // const privateKey = await PrivateKey.generate();
+    // const publicKey = privateKey.publicKey;
+
+    // const transaction = new AccountCreateTransaction()
+    // .setKey(publicKey)
+    // .setInitialBalance(0)
+
+    // const txResponsecreate = await transaction.execute(client);
+
+    // //Request the receipt of the transaction
+    // const receiptCreate = await txResponsecreate.getReceipt(client);
+
+    // //Get the account ID
+    // const hederaAccountID = receiptCreate.accountId;
+
+    // console.log(hederaAccountID)
+
+    // ///TokenAssociateTransaction
+
+    // console.log("tokenId", tokenId);
+
+    // const tokentransactionAssociate = await new TokenAssociateTransaction()
+    // .setAccountId(hederaAccountID)
+    // .setTokenIds([tokenId])
+    // .freezeWith(client);
+
+    // const signTx = await tokentransactionAssociate.sign(privateKey);
+    // //Submit the transaction to a Hedera network 
+    // const txResponse = await signTx.execute(client);
+    // //Request the receipt of the transaction
+    // const receipt = await txResponse.getReceipt(client);
+    // //Get the transaction consensus status
+    // const transactionStatus = receipt.status;
+
+    // console.log(transactionStatus);
 
     const user = await guestClient.query(
       q.Create(q.Collection("User"), {
--- utils/hedera-treasury.js
+++ utils/hedera-treasury.js
@@ -6,6 +6,7 @@ const {
     AccountId,
 } = require("@hashgraph/sdk");
 
+export const tokenId = process.env.TOKENID;
 export const accountKey = PrivateKey.fromString(process.env.PRIVATE_KEY);
 export const accountId = AccountId.fromString(process.env.ACCOUNT_ID);
 export const client = Client.forTestnet().setOperator(accountId, accountKey);
