b53d178dd3e346b5970ce2fc0d96102645ea0c02
rpalakkal <roshan.palakkal@gmail.com>
2021-02-12 23:57:35 -0500

    adidas -> nike

--- pages/index.js
+++ pages/index.js
@@ -66,7 +66,7 @@ export default function Home({ token }) {
                 />
                 <Item
                   user={user}
-                  subtitle="Adidas"
+                  subtitle="Nike"
                   name="RBW"
                   price="50"
                   imgPath="https://images.unsplash.com/photo-1552346154-21d32810aba3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
