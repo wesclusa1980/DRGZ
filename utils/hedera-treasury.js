bf009aa2eeb7c335db22dd6cb016aa415b093363
wesclusa1980 <will.esclusa@icloud.com>
2021-03-15 08:31:59 -0400

    c

--- utils/hedera-treasury.js
+++ utils/hedera-treasury.js
@@ -7,5 +7,5 @@ const { Client, PrivateKey, AccountId } = require("@hashgraph/sdk");
 export const tokenId = process.env.TOKENID;
 export const accountKey = "302e020100300506032b657004220420063dbbf0d5c090006dd95fd651519e30a345d8a8c0f72d35901ae444014d220e";
 export const accountId = "0.0.385071";
-//export const client = Client.forTestnet().setOperator(accountId, accountKey);
+export const client = Client.forTestnet().setOperator(accountId, accountKey);
 export const PRIVATE_KEY = "302e020100300506032b657004220420063dbbf0d5c090006dd95fd651519e30a345d8a8c0f72d35901ae444014d220e"
