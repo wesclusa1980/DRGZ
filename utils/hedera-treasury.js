dc714a0cbec2628ca44a87bc2ae5b275ed7acb02
wesclusa1980 <will.esclusa@icloud.com>
2021-03-15 15:54:56 -0400

    dial

--- utils/hedera-treasury.js
+++ utils/hedera-treasury.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const { Client, PrivateKey, AccountId } = require("@hashgraph/sdk");
 
 // console.log("PK", process.env.PRIVATE_KEY)
-console.log("PK", "02e020100300506032b657004220420063dbbf0d5c090006dd95fd651519e30a345d8a8c0f72d35901ae444014d220e")
+//console.log("PK", "02e020100300506032b657004220420063dbbf0d5c090006dd95fd651519e30a345d8a8c0f72d35901ae444014d220e")
 export const tokenId = process.env.TOKENID;
 export const accountKey = PrivateKey.fromString(process.env.PRIVATE_KEY);
 export const accountId = AccountId.fromString(process.env.ACCOUNT_ID);
